48cf743caae908fe74bbcf123f4ef7a9f630c220
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-07-19 00:37:55 +0900

    lig example of rxjs is added http://liginc.co.jp/web/js/151272

--- src/ts/simple_observable.ts
+++ src/ts/simple_observable.ts
@@ -1,12 +1,10 @@
 "use strict";
 
-
-
 import {Observable} from "./../../node_modules/rxjs/Rx";
 
 class simple_observable{
 
-	constructor();
+	constructor(){};
 
 	KeyupObservable(): void {
 		var key$ = Observable.fromEvent(document, 'keyup')
@@ -42,8 +40,8 @@ class simple_observable{
 		/*****lesson 52 map operator****/
 
 		const firstLesson$ = lessons$.map(lessons =>{
-			return lessons[0]
-			)
+			return lessons[0];
+		})
 		firstLesson$.subscribe(
 			lesson => {
 				console.log("first lesson is" + lesson);
@@ -63,17 +61,84 @@ class simple_observable{
 		const mouse$ = Observable.fromEvent(document, 'mousemove')
 							.filter((move:MouseEvent) =>{
 								return move.clientY >=200
-							}));
+							});
 		const combined$ = Observable.combineLatest(mouse$, click$);
 		combined$.subscribe(
 			(combined) =>{
 				console.log(combined[0]);
 			}
 		);
+	}
 
+	ArrayObservable(): any{
+		const num_arr$ = Observable.from([1,2,3,4,5,6,7,8])
+			.filter((index: number) : boolean => (index % 2) ==0)
+			.map( (num) : number =>{ return num*2});
+
+			num_arr$.subscribe(num =>{
+				console.log(num);
+			});
+	}
+
+	Lig_sub_ob(): any{
+
+/* implementation of this way is not possible any more
+
+		const observer = observer.create(
+			(num) : any =>{
+				return console.log("onNext:" + num);
+			},
+			(error) => {
+				return console.log("onError" + error);
+			},
+			() => {
+				return console.log("oncompleted");
+			}
+		);
+*/
+		const observable = Observable.from([1,2,3,4,5,6,7,8])
+			.filter((index: number) : boolean => (index % 2) ==0)
+			.map( (num) : number =>{ return num*2});
+		
+		observable.subscribe(
+			(num) : any =>{
+				return console.log("onNext:" + num);
+			},
+			(error) => {
+				return console.log("onError" + error);
+			},
+			() => {
+				return console.log("oncompleted");
+			}
+		);
 
 
 	}
+
+
+	Lig_sub_ob_2(): any{
+
+		const observable = Observable.from([1,2,3,4,5,6,7,8])
+			.filter((index: number) : boolean => (index % 2) ==0)
+			.map( (num) : number =>{ return num*3});
+		
+		
+		let onNext : any = function(num){
+			return console.log("onNext:" + num);
+		}
+		let onError : any = function(error){
+			return console.log("onError" + error);
+		}
+		let onComplete : any = function(){
+			return console.log("oncompleted");
+		}
+
+		observable.subscribe(onNext,onError, onComplete);
+
+	}
+
+
+
 
 
 }
